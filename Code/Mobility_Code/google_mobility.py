e4d44cf15d96f42cce0327d1637a7b3caec03a7c
Xucheng Tang <haroldtangtxc@gmail.com>
2020-04-27 01:45:15 -0400

    Update google_mobility.py

--- Code/Mobility_Code/google_mobility.py
+++ Code/Mobility_Code/google_mobility.py
@@ -1,12 +1,10 @@
-import sys
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark import SparkContext
 
 sc = SparkContext()
 spark = SparkSession.builder.appName("project").config("spark.some.config.option", "some-value").getOrCreate()
-google_mobility_df = spark.read.format('csv').options(header = 'true', inferschema = 'true').load(sys.argv[1])
+google_mobility_df = spark.read.format('csv').options(header = 'true', inferschema = 'true').load("/user/xt544/google_mobility-country.csv")
 google_mobility_df.createOrReplaceTempView("google")
 drop_country = ['country','countryiso3']
 united_states = spark.sql("select * from google where country = 'United States' and countryiso3 = 'USA'").drop(*drop_country)
