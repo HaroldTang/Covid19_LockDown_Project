b23eeadf016edfab5ea81e671e0967c8df362101
Jiaying Li <49607147+jiaying-Lee@users.noreply.github.com>
2020-05-10 04:39:54 -0400

    visualization of tweets, unemployment, stock

---
+++ Visualizaiton/unemployment rate/all rate/figure.js
@@ -0,0 +1,640 @@
+var figure = {
+    "data": [
+        {
+            "line": {
+                "shape": "spline"
+            },
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "C"
+                }
+            },
+            "mode": "lines",
+            "name": "Nonagriculture industries",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:4a1603",
+            "y": [
+                "Nonagriculture industries",
+                "15.6",
+                "4.6",
+                "3.7",
+                "3.8",
+                "3.2",
+                "3.2"
+            ],
+            "yaxis": "y",
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "D"
+                }
+            },
+            "mode": "lines",
+            "name": "Mining, quarrying, and oil and gas extraction",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:8c0d6a",
+            "y": [
+                "Mining, quarrying, and oil and gas extraction",
+                "10.2",
+                "6.2",
+                "5.4",
+                "1.9",
+                "3.8",
+                "3.2"
+            ],
+            "visible": true,
+            "stackgroup": null
+        },
+        {
+            "line": {
+                "color": "rgb(228, 224, 95)"
+            },
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "E"
+                }
+            },
+            "mode": "lines",
+            "name": "Construction",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:ff8e44",
+            "y": [
+                "Construction",
+                "16.6",
+                "6.9",
+                "5.5",
+                "5.4",
+                "5.0",
+                "4.4"
+            ],
+            "stackgroup": null
+        },
+        {
+            "line": {
+                "color": "rgb(151, 208, 168)"
+            },
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "F"
+                }
+            },
+            "mode": "lines",
+            "name": "Manufacturing",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:16711b",
+            "y": [
+                "Manufacturing",
+                "13.2",
+                "4.2",
+                "3.9",
+                "3.4",
+                "2.7",
+                "2.7"
+            ],
+            "stackgroup": null
+        },
+        {
+            "line": {
+                "color": "rgb(73, 35, 228)"
+            },
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "G"
+                }
+            },
+            "mode": "lines",
+            "name": "Durable goods manufacturing",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:506058",
+            "y": [
+                "Durable goods manufacturing",
+                "15.1",
+                "3.5",
+                "3.6",
+                "3.2",
+                "2.5",
+                "2.9"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "H"
+                }
+            },
+            "mode": "lines",
+            "name": "Nondurable goods manufacturing",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:01dd74",
+            "y": [
+                "Nondurable goods manufacturing",
+                "10.2",
+                "5.3",
+                "4.3",
+                "3.7",
+                "3.1",
+                "2.4"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "I"
+                }
+            },
+            "mode": "lines",
+            "name": "Wholesale and retail trade",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:21f0ab",
+            "y": [
+                "Wholesale and retail trade",
+                "17.1",
+                "4.9",
+                "4.2",
+                "4.7",
+                "3.6",
+                "3.4"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "J"
+                }
+            },
+            "mode": "lines",
+            "name": "Transportation and utilities",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:310074",
+            "y": [
+                "Transportation and utilities",
+                "13.5",
+                "5.0",
+                "3.4",
+                "3.0",
+                "2.6",
+                "3.0"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "K"
+                }
+            },
+            "mode": "lines",
+            "name": "Information",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:93a9d3",
+            "y": [
+                "Information",
+                "11.0",
+                "1.8",
+                "2.6",
+                "2.3",
+                "1.9",
+                "1.6"
+            ],
+            "visible": true,
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "L"
+                }
+            },
+            "mode": "lines",
+            "name": "Financial activities",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:7a9ca1",
+            "y": [
+                "Financial activities",
+                "5.4",
+                "2.2",
+                "1.6",
+                "2.5",
+                "2.3",
+                "2.1"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "M"
+                }
+            },
+            "mode": "lines",
+            "name": "Professional and business services",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:d37d9a",
+            "y": [
+                "Professional and business services",
+                "9.8",
+                "4.8",
+                "4.4",
+                "4.1",
+                "3.1",
+                "3.6"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "N"
+                }
+            },
+            "mode": "lines",
+            "name": "Education and health services",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:85f005",
+            "y": [
+                "Education and health services",
+                "10.9",
+                "3.2",
+                "2.4",
+                "2.4",
+                "2.4",
+                "2.3"
+            ],
+            "visible": "legendonly",
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "O"
+                }
+            },
+            "mode": "lines",
+            "name": "Leisure and hospitality",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:9029ae",
+            "y": [
+                "Leisure and hospitality",
+                "39.3",
+                "8.1",
+                "5.7",
+                "5.9",
+                "5.0",
+                "4.9"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "P"
+                }
+            },
+            "mode": "lines",
+            "name": "Other services",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:927231",
+            "y": [
+                "Other services",
+                "23.0",
+                "3.9",
+                "2.8",
+                "3.7",
+                "3.2",
+                "3.1"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "Q"
+                }
+            },
+            "mode": "lines",
+            "name": "Agriculture, forestry, fishing, and hunting",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:24dfca",
+            "y": [
+                "Agriculture, forestry, fishing, and hunting",
+                "9.6",
+                "8.3",
+                "11.0",
+                "12.5",
+                "9.6",
+                "7.7"
+            ],
+            "stackgroup": null
+        },
+        {
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "R"
+                }
+            },
+            "mode": "lines",
+            "name": "All Industries Government Wage & Salary Workers",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:11e2a0",
+            "y": [
+                "All Industries Government Wage & Salary Workers",
+                "9.4",
+                "2.2",
+                "1.6",
+                "2.1",
+                "1.8",
+                "1.8"
+            ],
+            "stackgroup": null
+        },
+        {
+            "line": {
+                "color": "rgb(236, 208, 227)"
+            },
+            "meta": {
+                "columnNames": {
+                    "x": "A",
+                    "y": "S"
+                }
+            },
+            "mode": "lines",
+            "name": "Self-employed unincorporated, and unpaid family workers",
+            "type": "scatter",
+            "xsrc": "jes_lee:1:cead98",
+            "x": [
+                "",
+                "Apr 2020",
+                "Mar 2020",
+                "Feb 2020",
+                "Jan 2020",
+                "Dec 2019",
+                "Nov 2019"
+            ],
+            "ysrc": "jes_lee:1:49ef59",
+            "y": [
+                "Self-employed unincorporated, and unpaid family workers",
+                "9.7",
+                "3.4",
+                "3.2",
+                "3.6",
+                "2.7",
+                "2.1"
+            ],
+            "stackgroup": null
+        }
+    ],
+    "layout": {
+        "title": {
+            "text": "Unemployment Rate"
+        },
+        "xaxis": {
+            "type": "category",
+            "range": [
+                6,
+                0
+            ],
+            "ticks": "",
+            "title": {
+                "font": {
+                    "color": "#4D5663"
+                },
+                "text": ""
+            },
+            "tickfont": {
+                "color": "#4D5663"
+            },
+            "autorange": false,
+            "gridcolor": "#E1E5ED",
+            "showspikes": false,
+            "tickprefix": "",
+            "ticksuffix": "",
+            "rangeslider": {
+                "range": [
+                    15,
+                    0
+                ],
+                "yaxis": {},
+                "visible": false,
+                "autorange": true
+            },
+            "zerolinecolor": "#E1E5ED"
+        },
+        "yaxis": {
+            "type": "linear",
+            "range": [
+                -0.494444444444444,
+                41.39444444444444
+            ],
+            "title": {
+                "font": {
+                    "color": "#4D5663"
+                },
+                "text": "Unemployment Rate"
+            },
+            "showline": false,
+            "tickfont": {
+                "color": "#4D5663"
+            },
+            "zeroline": false,
+            "autorange": true,
+            "gridcolor": "#E1E5ED",
+            "tickprefix": "",
+            "zerolinecolor": "#E1E5ED"
+        },
+        "legend": {
+            "x": 1.0094117647058825,
+            "y": 1.1139705882352942,
+            "font": {
+                "color": "#4D5663"
+            },
+            "bgcolor": "#F5F6F9",
+            "traceorder": "normal"
+        },
+        "autosize": true,
+        "dragmode": "zoom",
+        "plot_bgcolor": "#F5F6F9",
+        "paper_bgcolor": "#F5F6F9"
+    },
+    "frames": []
+}
